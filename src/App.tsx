1bddb952abbf9edc1df3f5d34c37baa32c579cbc
L A Minora <leonardo.minora@gmail.com>
2024-03-18 15:44:11 -0300

    adicionado código do dia 18-03

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,77 @@ const MeuBotao = (props: any) => {
   return <button>{props.titulo}</button>;
 }
 
+const Usuario = (props: any) => {
+  return (<h3>{props.nome}</h3>);
+}
+
+const ListaDeTarefas = (props: any) => {
+  return (
+    <>
+      <h4>Tarefas como lista de objetos</h4>
+      <ul>
+        {
+          props.dados.map((item) => {
+            return <ItemTarefa key={item.id} titulo={item.titulo} />
+          })
+        }
+      </ul>
+    </>
+  );
+}
+
+const ItemTarefa = (props: any) => {
+  return (<li>{props.titulo}</li>);
+}
+
 const App = () => {
-    return (
-      <div className="aplicacao">
-        <h1>Bem vindo ao mundo React</h1>
-        <MeuBotao titulo="Meu primeiro botão"/>
-        <MeuBotao titulo="Segundo botão" />
-        <MeuBotao titulo="Último botão" />
-      </div>
-    );
-  }
-  
-  export default App;
-  
+  const usuario = { nome: "minora" };
+  const tarefas = [
+    "Minha tarefa inicial",
+    "Segunda tarefa do dia"
+  ];
+
+  const tarefasMundoReal = [
+    {
+      id: 1,
+      titulo: "criar interface react",
+      concluido: false,
+    },
+    {
+      id: 2,
+      titulo: "conectar a uma  api",
+      concluido: false,
+    },
+    {
+      id: 3,
+      titulo: "aprender sobre autenticação",
+      concluido: false,
+    },
+  ];
+
+  return (
+    <div className="aplicacao">
+      <h1>Infoweb - React</h1>
+      {
+        usuario ?
+          (<Usuario nome={usuario.nome} />) :
+          (<MeuBotao titulo="Login" />)
+      }
+
+      {
+        !usuario && (<MeuBotao titulo="Login" />)
+      }
+      <h4>Tarefas como lista de string</h4>
+      <ul>
+        {
+          tarefas.map((item, indice) => {
+            return (<li key={indice}>{item}</li>)
+          })
+        }
+      </ul>
+      <ListaDeTarefas dados={tarefasMundoReal}/>
+    </div>
+  );
+}
+
+export default App;
